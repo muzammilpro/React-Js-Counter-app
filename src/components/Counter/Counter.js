471e7ad31f03193e8ba646c0470ec8767c343fdd
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-23 23:19:55 +0500

    Do Some Changes

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import './Counter.css'
+import { useState } from 'react'
+
 
 export default function Counter() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [counter , setCounter]= useState(0)
+    const increament = ()=>{
+        setCounter(counter +1)
+    }
+    const decreament = ()=>{
+        setCounter(counter - 1)
+    }
+    const reset = ()=>{
+        setCounter(0)
+    }
+    return (
+
+        
+        <div className='MAIN'>
+            <div className='btndiv'>
+                <button 
+                onClick={increament}
+                className='btn'
+                >+</button>
+                <h1>{counter}</h1>
+                <button className='btn'
+                onClick={decreament}
+                >-</button>
+
+
+            </div>
+            <button
+            onClick={reset}
+            >Reset</button>
+
+
+        </div>
+    )
 }
+
+
