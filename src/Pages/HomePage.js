93d6f2a8eaf187b24b223450be165cf5e8d0477b
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-23 23:13:11 +0500

    Do Some Changes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Counter from '../components/Counter/Counter'
 
 export default function HomePage() {
   return (
     <div>
-      
+      <Counter/>
     </div>
   )
 }
